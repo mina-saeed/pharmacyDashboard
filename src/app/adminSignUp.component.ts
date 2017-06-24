feefc499164d37a7cc1682fbcc01fcee2d8ecfb6
mahmoud <mahmoud@beeieg.com>
2017-06-24 06:03:47 +0200

    Admin signup design with angular2

---
+++ src/app/adminSignUp.component.ts
@@ -0,0 +1,13 @@
+import { Component} from '@angular/core'
+
+@Component({
+
+	selector: 'app-adminsignup',
+
+	templateUrl: './adminsignup.component.html',
+
+})
+export class admin {
+
+	title="admin"
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { admin } from './adminSignUp.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    admin
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [admin]
 })
 export class AppModule { }
