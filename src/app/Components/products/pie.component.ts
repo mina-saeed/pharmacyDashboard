056684603adf2e12a7dd2b140be1dd58ab77278b
rehamayman964 <reham.saad@student.guc.edu.eg>
2017-08-31 00:45:29 +0200

    ChangePassword

--- src/app/Components/products/pie.component.ts
+++ src/app/Components/products/pie.component.ts
@@ -1,12 +1,23 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import { FlashMessagesService } from 'angular2-flash-messages';
- 
+import { users } from '../../shared/users.service'
+
 @Component({
   selector: 'pie-chart-demo',
-  templateUrl: './pie.component.html'
+  templateUrl: './pie.component.html',
+  providers: [ users]
+  
 })
-export class pie {
+export class pie implements OnInit{
+  private history: any  
+  constructor(private users: users) { }
+  ngOnInit(){
+    this.users.pharmacyHistory().subscribe(res => {
+      console.log(res)
+      return this.history = res; // card commented in html 
+    })
+  }
   // Pie
   public pieChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales'];
   public pieChartData:number[] = [300, 500, 100];
